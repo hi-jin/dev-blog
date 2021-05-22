2224cd8d64bfa2d06975a9fd147b4eb996554e75
hi-jin <crushed7@o.cnu.ac.kr>
2021-05-22 05:04:12 +0000

    게시글 작성 기능 구현 완료. (아직 입력값 검증 X)

--- user/write.php
+++ user/write.php
@@ -2,9 +2,22 @@
     session_start();
     if ( $_SESSION['is_admin'] == 1 ) {
         $conn = mysqli_connect("localhost", "root", "1284", "blog", 3306);
-        $sql = "INSERT INTO board(title, content, user_name, date, tag)";
-        $msg = $_POST['posting_text'];
-        echo("<script>alert('{$msg}');</script>");
+        
+        date_default_timezone_set("Asia/Seoul");
+        $title = $_POST['posting_title'];
+        $content = $_POST['posting_text'];
+        $user_name = $_SESSION['user_name'];
+        $date = date("Y-m-d H:i:s");
+        $tag = $_POST['tag'];
+        
+        $sql = "INSERT INTO board(title, content, user_name, date, tag) VALUES('{$title}', '{$content}', '{$user_name}', '{$date}', '{$tag}')";
+        $result = mysqli_query($conn, $sql);
+        
+        if ( $result ) {
+            echo('<script>alert("게시글이 정상적으로 작성되었습니다.");</script>');
+        } else {
+            echo('<script>alert("게시글 작성 실패");</script>');
+        }
     } else {
         $msg = "로그인 후 작성할 수 있습니다.";
         echo("<script>alert('{$msg}'); location.href='/user/login.php'; </script>");
